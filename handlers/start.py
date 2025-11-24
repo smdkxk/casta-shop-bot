1f8181fe7e5fb8158b8444ce230514e212309a38
Yaroslav Nechvidov <nevidovv22@gmail.com>
2025-11-24 15:49:24 +0300

    Use common main_menu_kb on /start

--- handlers/start.py
+++ handlers/start.py
@@ -1,24 +1,19 @@
 from aiogram import Router, types
 from aiogram.filters import CommandStart
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from .menu import main_menu_kb
+
+
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def start_cmd(message: types.Message):
-    kb = ReplyKeyboardMarkup(
-        keyboard=[
-            [KeyboardButton(text="🛍 Каталог")],
-            [KeyboardButton(text="📏 Размеры")],
-            [KeyboardButton(text="ℹ О магазине")],
-        ],
-        resize_keyboard=True
-    )
-
     await message.answer(
         "Привет! Это *Casta Shop Bot*.\n\n"
         "Выбери действие в меню:",
         parse_mode="Markdown",
-        reply_markup=kb
+        reply_markup=main_menu_kb,  # 👈 тут уже общая клава из menu.py
     )
+
